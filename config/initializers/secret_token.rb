3570c75853ecc07387c480a18f1902c4732a37f0
Iqbal Farabi <iqbal@starqle.com>
2013-01-05 21:43:21 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mapstories::Application.config.secret_token = '12b19492ff8af00fd525bf296a975a985dcea478eb8c664c57d6bb1ff3c744b2cdf3bc178b70c47cb7c606dd74118e229b5f0eccc114786742a88a7e046d3df3'
