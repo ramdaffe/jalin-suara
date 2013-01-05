0429d15123458e0599230b31043bd256b60fd313
Iqbal Farabi <iqbal@starqle.com>
2013-01-06 00:50:09 +0700

    Using twitter-bootstrap for UI

---
+++ db/migrate/20130105172442_create_provinces.rb
@@ -0,0 +1,11 @@
+class CreateProvinces < ActiveRecord::Migration
+  def change
+    create_table :provinces do |t|
+      t.string :name
+      t.decimal :latitude
+      t.decimal :longitude
+
+      t.timestamps
+    end
+  end
+end
