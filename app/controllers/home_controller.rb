dc3a929f2093b9c093194f10c454489f375948c8
Iqbal <iqbal@starqle.com>
2013-01-27 17:46:13 +0700

    initial trial for three columns layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
 class HomeController < ApplicationController
   # GET /
   def index
-  	@featured_activities_left = Activity.find(:all, :order => 'id DESC', :limit => 2)
-    @featured_activities_right = Activity.find(:all, :order => 'id DESC', :offset => 2, :limit => 4)
     @activities = Activity.all
     @json = Activity.all.to_gmaps4rails
 
