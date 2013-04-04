d0d2d9d73ba294379c87444bc418ebeb39712ca5
Iqbal <iqbal@starqle.com>
2013-04-05 00:34:48 +0700

    Reverting changes back, including removing Unicorn

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -44,7 +44,7 @@ class HomeController < ApplicationController
       @search = Activity.search(params[:search])
       activities = @search.all
     else
-      activities = Activity.all
+      activities = Activity.find(:all, :limit => 1000)
     end
     @json = activities.to_gmaps4rails
 
