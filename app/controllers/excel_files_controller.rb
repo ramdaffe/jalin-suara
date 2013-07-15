ea2dbd1dc4d100977be42162a145af16a9a21bdb
Iqbal <iqbal@starqle.com>
2013-07-16 01:50:05 +0700

    still trying to fix paperclip problem

--- app/controllers/excel_files_controller.rb
+++ app/controllers/excel_files_controller.rb
@@ -9,8 +9,8 @@ class ExcelFilesController < ApplicationController
     respond_to do |format|
       if @excel_file.save
         redirect_path = activities_path
-        csv_file = File.read(@excel_file.document)
-        csv = CSV.parse(csv_file, :headers => true)
+        csv_file = open(@excel_file.document)
+        csv = CSV.parse(csv_file.read, :headers => true)
         csv.each do |row|
           province_name = row[1]
           district_name = row[3]
