873eeb81ef360eb4a0e919bff8eda7f31a29ed40
Iqbal <iqbal@starqle.com>
2013-08-10 21:08:22 +0800

    Working Ajax sort, search and paginate for Province model

--- app/assets/javascripts/ajax_table.js
+++ app/assets/javascripts/ajax_table.js
@@ -1,4 +1,17 @@
-$(document).on('click','#provinces th a',function(){
-  $.getScript(this.href);
-  return false;
+$(function(){
+  $(document).on('click','#provinces th a, #provinces .pagination a',function(){
+    $.getScript(this.href);
+    return false;
+  });
+
+  // Search form
+  $('#provinces_search').submit(function () {
+    $.get(this.action, $(this).serialize(), null, 'script');
+    return false;
+  });
+
+  $('#provinces_search input').keyup(function () {
+    $.get($('#provinces_search').attr('action'), $('#provinces_search').serialize(), null, 'script');
+    return false;
+  });
 });
