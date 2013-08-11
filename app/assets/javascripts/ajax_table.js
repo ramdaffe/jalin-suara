0d2d8c4ad32af8328f10ca24af3dbf0a4047cb86
Iqbal <iqbal@starqle.com>
2013-08-11 11:32:03 +0800

    Minor fix, simplifying Js file for Ajax table

--- app/assets/javascripts/ajax_table.js
+++ app/assets/javascripts/ajax_table.js
@@ -1,28 +1,8 @@
 $(function(){
-  $(document).on('click','#provinces th a, #provinces .pagination a',function(){
-    $.getScript(this.href);
-    return false;
-  });
-
-  $('#provinces_search').submit(function () {
-    $.get(this.action, $(this).serialize(), null, 'script');
-    return false;
-  });
-
   // $('#provinces_search input').keyup(function () {
   //   $.get($('#provinces_search').attr('action'), $('#provinces_search').serialize(), null, 'script');
   //   return false;
   // });
-
-  $(document).on('click','#districts th a, #districts .pagination a',function(){
-    $.getScript(this.href);
-    return false;
-  });
-
-  $('#districts_search').submit(function () {
-    $.get(this.action, $(this).serialize(), null, 'script');
-    return false;
-  });
 
   // $('#districts_search input').keyup(function () {
   //   $.get($('#districts_search').attr('action'), $('#districts_search').serialize(), null, 'script');
