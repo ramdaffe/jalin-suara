2f1839e1d3677a3f5b8f53ab2d08d7b27efa4dc7
Iqbal <iqbal@starqle.com>
2013-09-11 16:18:19 +0700

    Minor fix for Show Map page

--- app/models/subdistrict.rb
+++ app/models/subdistrict.rb
@@ -19,13 +19,15 @@ class Subdistrict < ActiveRecord::Base
     info = ""
     info << "<h5>#{self.name}</h5>"
     i = 0
-    
-    self.activities.each do |activity|
-      i += 1
-      info << "<b>#{i}. #{activity.name}</b><br/>"
-      info << "Jumlah Penerima Manfaat: #{activity.male_beneficiary + activity.female_beneficiary}<br/>"
-      info << "Jumlah BLM: #{activity.blm_amount}<br/>"
-      info << "<a href='/activities/#{activity.id}'>Lihat selengkapnya</a><br/><br/>"
+
+    if self.activities != nil
+      self.activities.each do |activity|
+        i += 1
+        info << "<b>#{i}. #{activity.name}</b><br/>"
+        info << "Jumlah Penerima Manfaat: #{activity.male_beneficiary + activity.female_beneficiary}<br/>"
+        info << "Jumlah BLM: #{activity.blm_amount}<br/>"
+        info << "<a href='/activities/#{activity.id}'>Lihat selengkapnya</a><br/><br/>"
+      end
     end
 
     return info
